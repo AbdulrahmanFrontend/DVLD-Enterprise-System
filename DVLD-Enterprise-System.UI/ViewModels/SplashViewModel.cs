855bedbe046aed4b0121c2e5dd89390580457901
AbdulrahmanFrontend <abdulrahman.abdelstar@gmail.com>
2026-05-23 17:44:13 +0300

    create project UI with wpf instead of win forms, build simple splash screen

---
+++ DVLD-Enterprise-System.UI/ViewModels/SplashViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVLD_Enterprise_System.UI.ViewModels
+{
+    public class SplashViewModel : BaseViewModel
+    {
+        private string _statusText;
+
+        public string StatusText
+        {
+            get => _statusText;
+            set
+            {
+                _statusText = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SplashViewModel()
+        {
+            StatusText = "Loading DVLD...";
+        }
+    }
+}
