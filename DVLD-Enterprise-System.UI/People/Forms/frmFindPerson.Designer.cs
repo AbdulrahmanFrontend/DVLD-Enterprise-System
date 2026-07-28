14a6f9b91dfb22f5767c414fc51f493739ef6f2f
AbdulrahmanFrontend <abdulrahman.abdelstar@gmail.com>
2026-07-28 03:53:04 +0300

    Building frmFindPerson

---
+++ DVLD-Enterprise-System.UI/People/Forms/frmFindPerson.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace DVLD_Enterprise_System.UI.People.Forms
+{
+    partial class frmFindPerson
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmFindPerson));
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.flpFooter = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnClose = new DevToolkit.BaseWinForms.Controls.CustomControls.ccButton();
+            this.PersonCard = new DVLD_Enterprise_System.UI.People.Controls.ctrlPersonCardWithFilter();
+            this.flpFooter.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Red;
+            this.lblTitle.Location = new System.Drawing.Point(0, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(866, 51);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Find Person";
+            this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // flpFooter
+            // 
+            this.flpFooter.AutoSize = true;
+            this.flpFooter.Controls.Add(this.btnClose);
+            this.flpFooter.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flpFooter.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+            this.flpFooter.Location = new System.Drawing.Point(0, 476);
+            this.flpFooter.Name = "flpFooter";
+            this.flpFooter.Padding = new System.Windows.Forms.Padding(3);
+            this.flpFooter.Size = new System.Drawing.Size(866, 52);
+            this.flpFooter.TabIndex = 2;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(99)))), ((int)(((byte)(235)))));
+            this.btnClose.ButtonStyle = DevToolkit.BaseWinForms.Theming.ButtonStyle.Primary;
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClose.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(203)))), ((int)(((byte)(213)))), ((int)(((byte)(225)))));
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(64)))), ((int)(((byte)(175)))));
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(78)))), ((int)(((byte)(216)))));
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Image = global::DVLD_Enterprise_System.UI.Properties.Resources.Close_32;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(736, 6);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Padding = new System.Windows.Forms.Padding(10, 0, 10, 0);
+            this.btnClose.Size = new System.Drawing.Size(121, 40);
+            this.btnClose.TabIndex = 0;
+            this.btnClose.Text = "Close";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // PersonCard
+            // 
+            this.PersonCard.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PersonCard.FilterEnabled = true;
+            this.PersonCard.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PersonCard.Location = new System.Drawing.Point(0, 51);
+            this.PersonCard.Name = "PersonCard";
+            this.PersonCard.ShowAddPerson = true;
+            this.PersonCard.Size = new System.Drawing.Size(866, 425);
+            this.PersonCard.TabIndex = 3;
+            // 
+            // frmFindPerson
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 528);
+            this.Controls.Add(this.PersonCard);
+            this.Controls.Add(this.flpFooter);
+            this.Controls.Add(this.lblTitle);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmFindPerson";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Find Person";
+            this.flpFooter.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.FlowLayoutPanel flpFooter;
+        private DevToolkit.BaseWinForms.Controls.CustomControls.ccButton btnClose;
+        private Controls.ctrlPersonCardWithFilter PersonCard;
+    }
+}
